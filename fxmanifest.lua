a98d759f35d94101ada78668dc134d4f8fafd37e
Samy Chaabi <xhackax47@gmail.com>
2022-10-10 17:01:29 +0200

    Upload script puff

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'bodacious'
+game 'gta5'
+lua54 'yes'
+
+name "metaverse_puff"
+author "xhackax47"
+
+server_script 'server.lua'
+client_script 'client.lua'
+shared_script 'config.lua'
+
+escrow_ignore {
+    'config.lua',
+    'shared.lua',
+}
